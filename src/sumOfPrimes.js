85e70ca6df6bb8a6547472da7b21c77ec17d7e33
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-03-12 14:06:08 -0300

    Simplified function 'isPrime': now it starts with low value divisions.

--- src/sumOfPrimes.js
+++ src/sumOfPrimes.js
@@ -1,13 +1,47 @@
 const main = () => {
     let inputs = process.argv
     if (inputs.length < 4) {
-        console.error("the initial and the final values must be passed")
+        console.error("the initial and the final values must be passed (both must be 0 or positive integer)")
     }
 
-    let start = parseInt(inputs[2]);
-    let end = parseInt(inputs[3]);
+    let start = parseInt(inputs[2].replaceAll(",", "").replaceAll(".", ""));
+    let end = parseInt(inputs[3].replaceAll(",", "").replaceAll(".", ""));
 
+    if (start < 0) {
+        start = 0
+    }
+
+    if (end < 0) {
+        end *= -1
+    }
+
+    if (start > end) {
+        [start, end] = [end, start]
+    }
+
+    sum_of_primes = sumOfPrimes(start, end)
+
+    console.log(`Sum of the primes between ${markNumber(start)} and ${markNumber(end)}: ${markNumber(sum_of_primes)}`)
+    return sum_of_primes
+}
+
+const sumOfPrimes = (start, end) => {
     return getListOfPrimes(start, end).reduce(sum)
+}
+
+const markNumber = (number, mark=",") => {
+    number = number.toString()
+    mark = mark[0]
+    let marked_number = ""
+    let count = 1
+    for (let i = number.length-1; i >= 0; i--) {
+        marked_number = number[i] + marked_number
+        if (count % 3 == 0 && count < number.length) {
+            marked_number = mark + marked_number
+        }
+        count += 1
+    }
+    return marked_number
 }
 
 const getListOfPrimes = (start, end) => {
@@ -23,10 +57,10 @@ const getListOfPrimes = (start, end) => {
 const sum = (x, y) => x + y;
 
 const isPrime = (n) => {
-    if (n === 1) {
+    if (n === 1 || n === -1) {
         return false;
     }
-    for (let i = n-1; i > 1; i--) {
+    for (let i = 2; i < n -1; i++) {
         if (n % i === 0) {
             return false;
         }
