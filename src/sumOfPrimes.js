f44d863eba4a7b90a71fe9c88f69cfbb91bc506b
tomasfn87 <tomas.nalle@alumni.usp.br>
2022-05-07 16:50:54 -0300

    Changed file 'src/sumOfPrimes.js': 1): function 'main' was moved to 'main/main.js'. 2): exported functions 2.1): 'sumOfPrimes', 2.2): 'markNumber', 2.3): 'getListOfPrimes', 2.4): 'isPrime' to write tests and use the functions in '../main/main.js'.

--- src/sumOfPrimes.js
+++ src/sumOfPrimes.js
@@ -1,35 +1,8 @@
-const main = () => {
-    let inputs = process.argv
-    if (inputs.length < 4) {
-        console.error("the initial and the final values must be passed (both must be 0 or positive integer)")
-    }
-
-    let start = parseInt(inputs[2].replaceAll(",", "").replaceAll(".", ""));
-    let end = parseInt(inputs[3].replaceAll(",", "").replaceAll(".", ""));
-
-    if (start < 0) {
-        start = 0
-    }
-
-    if (end < 0) {
-        end *= -1
-    }
-
-    if (start > end) {
-        [start, end] = [end, start]
-    }
-
-    sum_of_primes = sumOfPrimes(start, end)
-
-    console.log(`Sum of the primes between ${markNumber(start)} and ${markNumber(end)}: ${markNumber(sum_of_primes)}`)
-    return sum_of_primes
-}
-
-const sumOfPrimes = (start, end) => {
+export const sumOfPrimes = (start, end) => {
     return getListOfPrimes(start, end).reduce(sum)
 }
 
-const markNumber = (number, mark=",") => {
+export const markNumber = (number, mark=",") => {
     number = number.toString()
     mark = mark[0]
     let marked_number = ""
@@ -44,7 +17,7 @@ const markNumber = (number, mark=",") => {
     return marked_number
 }
 
-const getListOfPrimes = (start, end) => {
+export const getListOfPrimes = (start, end) => {
     let listOfPrimes = [];
     for (let i = start; i <= end; i++) {
         if (isPrime(i)) {
@@ -56,19 +29,17 @@ const getListOfPrimes = (start, end) => {
 
 const sum = (x, y) => x + y;
 
-const isPrime = (n) => {
+export const isPrime = (n) => {
     if (n === 1 || n === -1) {
         return false;
     }
     if (n % 2 === 0 && n !== 2) {
         return false
     }
-    for (let i = 3; i < n; i += 2) {
+    for (let i = 3; i <= n-1; i += 2) {
         if (n % i === 0) {
             return false;
         }
     }
     return true;
 }
-
-console.log(main())
